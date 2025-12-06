c28783960309e6bc1ed84742d2e8ee272e67ad33
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-12-06 17:18:41 -0300

    Primer commit

--- kbn-front/src/App.jsx
+++ kbn-front/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+import Login from "./components/Login";
+import AdminDashboard from "./components/AdminDashboard";
+import InstructorForm from "./components/InstructorForm";
+import Register from "./components/Register";
+import ReporteEstadisticas from "./components/ReporteEstadisticas";
+import UserManagement from "./components/UserManagement";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userLogged, setUserLogged] = useState(false);
+
+  // Cuando el usuario inicia sesión, se actualiza el estado
+  const handleLoginSuccess = () => {
+    setUserLogged(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!userLogged ? (
+        // 👇 Primera pantalla: LOGIN
+        <Login onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        // 👇 Pantalla después de iniciar sesión
+        <AdminDashboard />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
