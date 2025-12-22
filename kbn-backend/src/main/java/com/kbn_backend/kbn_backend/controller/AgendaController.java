53253f1d0a011baf013fe7ed1cceac9c50be0f03
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2025-12-22 01:02:54 -0300

    segundo

--- kbn-backend/src/main/java/com/kbn_backend/kbn_backend/controller/AgendaController.java
+++ kbn-backend/src/main/java/com/kbn_backend/kbn_backend/controller/AgendaController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "https://kbn-admin.vercel.app", allowCredentials = "true")
 @RequestMapping("/api/agenda")
 public class AgendaController {
 
